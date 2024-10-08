68a1c53850bd5ecdfaa801d6c0ef09edab710067
Frederic ROUSSET <frederic.rousset@hotmail.fr>
2024-10-08 08:53:37 +0200

    Création des layouts, homepage et routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { GalleryProjetComponent } from './pages/gallery-projet/gallery-projet.component';
+import { DetailsComponent } from './pages/details/details.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'gallery-projet', component: GalleryProjetComponent },
+  { path: 'details', component: DetailsComponent }
+];
+
