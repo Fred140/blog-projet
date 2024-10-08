01adbbdb7a2d633226c4bb7a3c87f37f4ffafd9d
Frederic ROUSSET <frederic.rousset@hotmail.fr>
2024-10-08 08:20:35 +0200

    Installation du projet et de firebase

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,13 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideFirebaseApp(() =>
+    initializeApp({"projectId":"ang-blog-19e44","appId":"1:961330352047:web:f512d1fe40df07919f295a",
+      "storageBucket":"ang-blog-19e44.appspot.com","apiKey":"AIzaSyDCEa8LxutBlrplQa76FeZPoZAodpG9oAc",
+      "authDomain":"ang-blog-19e44.firebaseapp.com","messagingSenderId":"961330352047"})), provideAuth(() => getAuth()), provideFirestore(() => getFirestore())]
 };
